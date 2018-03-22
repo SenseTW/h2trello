6819fe0d39ded54e9d5a1d81cd26a0e83e064181
Pomin Wu <pomin5@gmail.com>
2018-03-22 13:27:56 +0800

    Refactored sensetw.applications

---
+++ sensetw/applications/h2trello.py
@@ -0,0 +1,59 @@
+
+import os
+import pickle
+
+db_path = os.path.join(os.path.dirname(__file__), "../../db/h2trello.db")
+def send(h, t, check_fn, limit=None):
+    """
+    Send Hypothesis annotations to Trello as cards.
+    """
+    inboxes = t.get_lists()
+    labels = t.get_labels()
+
+    sent_cards = []
+
+    for card in h.annotations():
+        if not check_fn(card):
+            continue
+        tid = t.post(card,
+                     labels=[labels[card.source_type]
+                             ] if card.source_type in labels else [],
+                     list_id=inboxes[card.source_type])
+        card.trello_id = tid
+        sent_cards.append(card)
+
+        if limit is not None and len(sent_cards) >= limit:
+            break
+
+    return sent_cards
+
+
+
+def save_log(log, filepath=None):
+    if filepath is None:
+        filepath = db_path
+    with open(filepath, "wb") as fh:
+        pickle.dump(log, fh)
+
+def load_log(filepath=None):
+    if filepath is None:
+        filepath = db_path
+    try:
+        with open(filepath, "rb") as fh:
+            return pickle.load(fh)
+    except FileNotFoundError:
+        return []
+
+def find_log_by_trello_id(log, tid):
+    for i, card in enumerate(log):
+        if card.trello_id == tid:
+            return i
+    return -1
+
+
+def find_log_by_hypothesis_id(log, hid):
+    for i, card in enumerate(log):
+        if card.hypothesis_id == hid:
+            return i
+    return -1
+
---
+++ tests/test_h2trello.py
@@ -0,0 +1,48 @@
+
+import pytest
+from sensetw.applications import h2trello
+from sensetw import Card
+import os
+
+test_log_path = os.path.join(os.path.dirname(__file__), "h2trello.db")
+
+
+@pytest.fixture
+def h2trello_log():
+    yield h2trello.load_log(filepath=test_log_path)
+    try:
+        os.remove(test_log_path)
+    except FileNotFoundError:
+        pass
+
+
+def test_h2trello_log():
+    log = h2trello.load_log(filepath=test_log_path)
+    log.append(123)
+    h2trello.save_log(log, filepath=test_log_path)
+
+    log = h2trello.load_log(test_log_path)
+    assert log == [123]
+
+    os.remove(test_log_path)
+
+
+def test_h2trello_log_find(h2trello_log):
+    card = Card(
+        title="", quote="",
+        hypothesis_id=0, trello_id=1
+    )
+    h2trello_log.append(card)
+
+    r = h2trello.find_log_by_hypothesis_id(h2trello_log, 0)
+    assert r == 0
+
+    r = h2trello.find_log_by_hypothesis_id(h2trello_log, 1)
+    assert r == -1
+
+    r = h2trello.find_log_by_trello_id(h2trello_log, 1)
+    assert r == 0
+
+
+if __name__ == "__main__":
+    pytest.run([__file__])
