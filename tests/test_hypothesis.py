71c5bf89fd36699a01cfcee8549f427353a2c3cf
Pomin Wu <pomin5@gmail.com>
2018-03-24 00:19:25 +0800

    Fixed title sanitize tests

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -29,7 +29,7 @@ def test_hypothesis_search_group_url(h):
 
 def test_hypothesis_annotation_to_card(h):
     c = annotation_to_card(Annotation.from_json(annotation_json))
-    assert c.title == "政院推台灣AI行動計畫 四年至少360億元 | 財經焦點 | 產經 | 聯合新聞網"
+    assert c.title == "政院推台灣AI行動計畫 四年至少360億元   財經焦點   產經   聯合新聞網"
     assert c.quote == "行政院科技會報執行秘書蔡志宏今（18）日表示，今年推動AI（人工智慧）的預算在90億元以上，預算來自科技預算以及前瞻基礎建設的數位建設；之後三年也保證預算在90億至100億元左右，相當於四年至少投入360億元。"
     assert c.source_url == "https://O.sense.tw/ljSw7iv9Eeilw3M5xviqxw"
     assert c.source_type == "外部意見"
@@ -40,7 +40,7 @@ def test_hypothesis_annotation_to_card(h):
 
 def test_annotation_from_json():
     a = Annotation.from_json(annotation_json)
-    assert a.title == "政院推台灣AI行動計畫 四年至少360億元 | 財經焦點 | 產經 | 聯合新聞網"
+    assert a.title == "政院推台灣AI行動計畫 四年至少360億元   財經焦點   產經   聯合新聞網"
     assert a.quote == "行政院科技會報執行秘書蔡志宏今（18）日表示，今年推動AI（人工智慧）的預算在90億元以上，預算來自科技預算以及前瞻基礎建設的數位建設；之後三年也保證預算在90億至100億元左右，相當於四年至少投入360億元。"
     assert a.uri == "https://udn.com/news/story/7238/2937685"
     assert a.link == "https://O.sense.tw/ljSw7iv9Eeilw3M5xviqxw"
